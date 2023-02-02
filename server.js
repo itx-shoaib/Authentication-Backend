c679b5d3acd9711a4e20ca0ca045e74817d77653
itx-shoaib <shoaibjamil43@gmail.com>
2023-02-02 05:09:32 +0500

    Making server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv").config()
+const bodyparser = require("body-parser");
+const cors = require("cors")
+
+const app = express();
+
+// const dbconfig = require('./db')
+
+// Middlewear
+app.use(express.json())
+app.use(bodyparser.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors());
+
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Node server started by using nodemon on port ${port} `))
